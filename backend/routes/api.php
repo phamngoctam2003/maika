41d359211f976d24c193a00c82597b6860d6addc
Tam <ngoctampksv@gmail.com>
2025-05-19 18:36:50 +0700

    auto deploy

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\RoleController;
 
 Route::get('test-api', function () {
     return response()->json([
-        'message' => 'Api đã hoạt động thành công',
+        'message' => 'Api đã hoạt động thành công haha',
         'status' => 200,
     ]);
 });
