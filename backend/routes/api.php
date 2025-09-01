6153949308a1781c633c7d679e1ae3e35df3d21b
Pham Ngoc Tam <ngoctampksv@gmail.com>
2025-09-01 12:34:26 +0700

    update

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\Auth\ResetPasswordController;
 
 Route::get('test-api', function () {
     return response()->json([
-        'message' => 'Api đang hoạt động ok!',
+        'message' => 'Api đang hoạt động!',
         'data' => [
             'name' => 'Laravel API',
             'version' => '12.0.0',
